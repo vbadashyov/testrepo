6538bc97fd0272c1d6b5a2e014e1c4ccba9ad4dc
Виталий Бадашёв <v.badashev@mail.ru>
2022-11-11 13:06:18 +0300

    testcommit4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {namddddddde}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {namdddddddddddde}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
